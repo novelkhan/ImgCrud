a67428f63fbcdbf1edb5d634fa47b25790d276c2
novelkhan <novel4004@gmail.com>
2024-01-21 04:04:33 +0600

    Code is incomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  baseUrl = 'https://localhost:7295/api/employee';
+
+  constructor(private http: HttpClient) {}
+
+  getEmployees() {
+    return this.http.get<Employee[]>(this.baseUrl);
+  }
+
+  postEmployee(employee: Employee) {
+    return this.http.post<Employee>(this.baseUrl, employee);
+  }
+
+  deleteEmployee(id: string) {
+    return this.http.delete(this.baseUrl + '/' + id);
+  }
 }
